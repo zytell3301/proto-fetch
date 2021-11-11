5160dd95360c8b00aa419ef003c4a3714b6eafdf
Arshiya Kiani <zytell3301@gmail.com>
2021-11-12 01:11:58 +0330

    Proceed implementation of loadConfig method

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import "github.com/spf13/viper"
+
+type Config struct {
+	repository string
+}
+
 func main() {
 
+}
+
+func loadConfigs() (Config,error){
+	cfg := viper.New()
+	cfg.SetConfigName("proto-fetch")
+	cfg.SetConfigType("yaml")
+	cfg.AddConfigPath("./")
+	err := cfg.ReadInConfig()
+	switch err != nil {
+	case true:
+		return Config{},err
+	}
+
+	return Config{
+		repository: cfg.GetString("repository"),
+	}, nil
 }
